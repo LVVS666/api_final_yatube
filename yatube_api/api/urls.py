67d8a8ffcf70cbcb353309e01c209c51af58b175
Василий Логинов <pupok777vasya@gmail.com>
2022-12-09 22:09:17 +0700

    "start_final_project_vol.1"

--- yatube_api/api/urls.py
+++ yatube_api/api/urls.py
@@ -1,4 +1,21 @@
-from django.urls import path
+from django.urls import include, path
+from rest_framework import routers
+
+from api.views import CommentViewSet, GroupViewSet, PostViewSet
+
+
+router_v1 = routers.DefaultRouter()
+router_v1.register('posts', PostViewSet, basename='posts')
+router_v1.register('groups', GroupViewSet, basename='groups')
+router_v1.register(
+    r'^posts/(?P<post_id>\d+)/comments',
+    CommentViewSet,
+    basename='comments'
+)
+
 
 urlpatterns = [
+    path('auth/', include('djoser.urls')),
+    path('auth/', include('djoser.urls.jwt')),
+    path('v1/', include(router_v1.urls)),
 ]
--- yatube_api/api/views.py
+++ yatube_api/api/views.py
@@ -1,1 +1,36 @@
-# TODO:  Напишите свой вариант
+from django.shortcuts import get_object_or_404
+from rest_framework import viewsets
+from rest_framework.permissions import IsAuthenticated
+
+from posts.models import Group, Post
+from api.serializers import CommentSerializer, PostSerializer, GroupSerializer
+from api.permissions import IsAuthorReadOnly
+
+
+class PostViewSet(viewsets.ModelViewSet):
+    queryset = Post.objects.all()
+    serializer_class = PostSerializer
+    permission_classes = (IsAuthenticated, IsAuthorReadOnly)
+
+    def perform_create(self, serializer):
+        serializer.save(author=self.request.user)
+
+
+class GroupViewSet(viewsets.ReadOnlyModelViewSet):
+    queryset = Group.objects.all()
+    serializer_class = GroupSerializer
+
+
+class CommentViewSet(viewsets.ModelViewSet):
+    def get_queryset(self):
+        post_obj = get_object_or_404(Post, pk=self.kwargs.get('post_id'))
+        return post_obj.comments.all()
+
+    serializer_class = CommentSerializer
+    permission_classes = (IsAuthenticated, IsAuthorReadOnly)
+
+    def perform_create(self, serializer):
+        post = get_object_or_404(Post, pk=self.kwargs.get('post_id'))
+        serializer.save(
+            author=self.request.user,
+            post=post)
