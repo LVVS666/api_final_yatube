d868090ee8afaae598afc8ddb512cdf40421cc94
Василий Логинов <pupok777vasya@gmail.com>
2022-12-12 13:03:48 +0700

    "start_final_project_vol.1"

--- yatube_api/api/permissions.py
+++ yatube_api/api/permissions.py
@@ -2,6 +2,9 @@ from rest_framework.permissions import BasePermission, SAFE_METHODS
 
 
 class IsAuthorReadOnly(BasePermission):
+    def has_permission(self, request, view):
+        return request.user
+
     def has_object_permission(self, request, view, obj):
         if request.method in SAFE_METHODS:
             return True
