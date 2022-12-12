3d180a6788989742072b836a006c85765a6dbe57
Василий Логинов <pupok777vasya@gmail.com>
2022-12-12 12:56:44 +0700

    "start_final_project_vol.1"

--- yatube_api/api/views.py
+++ yatube_api/api/views.py
@@ -2,7 +2,9 @@ from django.shortcuts import get_object_or_404
 from rest_framework import viewsets
 from rest_framework.pagination import LimitOffsetPagination
 from rest_framework.permissions import IsAuthenticated
+from rest_framework import mixins
 from rest_framework import filters
+
 
 from posts.models import Group, Post, Comment
 from api.serializers import (CommentSerializer,
@@ -23,7 +25,8 @@ class PostViewSet(viewsets.ModelViewSet):
         serializer.save(author=self.request.user)
 
 
-class FollowViewSet(viewsets.ModelViewSet):
+class FollowViewSet(mixins.CreateModelMixin, mixins.ListModelMixin,
+                    viewsets.GenericViewSet):
     serializer_class = FollowSerializer
     permission_classes = (IsAuthenticated,)
     filter_backends = (filters.SearchFilter,)
